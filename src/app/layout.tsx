9b5b09481fc94d222ed57f831bef951177efe8b4
nicenathapong <nicenathapong@gmail.com>
2025-06-02 01:16:05 +0700

    feat: xxx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Sarabun } from "next/font/google";
+import { Sarabun } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const sarabun = Sarabun({
-  variable: "--font-sarabun",
   subsets: ["latin"],
-  weight: "400"
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 })
 
 export const metadata: Metadata = {
@@ -31,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${sarabun.variable} antialiased`}
+        className={`${sarabun.className} antialiased`}
       >
         {children}
       </body>
