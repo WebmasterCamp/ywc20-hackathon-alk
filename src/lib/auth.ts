f04eb32e9d3be6f33c0b2b20a587802d43772609
Nutchy <work@nutchy.dev>
2025-06-02 15:38:00 +0700

    fix: change domain

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -45,7 +45,7 @@ export const auth = betterAuth({
     // Hardcode base URL for production
     baseURL:
         process.env.NODE_ENV === "production"
-            ? "https://www.ประสานบุญ.ไทย"
+            ? "https://prasanboon.in.th"
             : process.env.BETTER_AUTH_URL,
     socialProviders: {
         google: {
