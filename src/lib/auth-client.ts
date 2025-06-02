887400daaf7f8f4832ec29c0aa6b54020eb0b2ac
nicenathapong <nicenathapong@gmail.com>
2025-06-02 09:58:01 +0700

    fix: test deploy

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,6 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
     baseURL:
         process.env.NODE_ENV === "production"
-            ? "https://eventmunk.nsys.site"
+            ? "https://ywc20-hackathon-alk.vercel.app"
             : "http://localhost:3000",
 });
